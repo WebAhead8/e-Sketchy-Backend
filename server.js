8329558896396039883f7a55daadf7ed645761b7
“Haneen <haneen.awad1996@gmail.com>
2021-02-23 20:37:21 +0200

    added

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require("express");
+const users = require("./handlers/users");
+
 const PORT = process.env.PORT || 3000;
 
 const server = express();
 
-server.get("/", (reg, res) => {
+server.get("/", (req, res) => {
   res.send("<h1> HELLOOOOOOOOOOO </h1>");
 });
+
+server.get("/users/:id", users.get);
+server.post("/users", users.post);
+server.post("/login", users.login);
+server.put("/users/:id", users.put);
 
 server.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
