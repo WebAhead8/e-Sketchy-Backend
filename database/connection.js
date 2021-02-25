90359c96b0afee3a6cd66bf70e4f5b41d5ba53a6
BoshEee <bosheee.shwi@gmail.com>
2021-02-25 11:25:17 +0200

    noth

--- database/connection.js
+++ database/connection.js
@@ -1,19 +1,17 @@
 const pg = require("pg");
 const dotenv = require("dotenv");
-
 dotenv.config();
 
-let connectionString = process.env.DEV_DATABASE_URL;
-
+let connectionString;
 if (process.env.NODE_ENV === "production") {
   connectionString = process.env.DATABASE_URL;
+} else {
+  connectionString = process.env.DEV_DATABASE_URL;
 }
 
 const db = new pg.Pool({
   connectionString,
-  ...(process.env.NODE_ENV === "production" && {
-    ssl: { rejectUnauthorized: false },
-  }),
+  ssl: { rejectUnauthorized: false },
 });
-console.log(connectionString);
+
 module.exports = db;
